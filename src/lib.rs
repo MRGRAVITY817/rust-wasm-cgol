24fc78e328b75bdcfec297f9808eeddfc4a625f6
Seong Hoon Wee <mrgravity817@gmail.com>
2021-04-04 11:27:31 +0900

    My first web assembly with rust

---
+++ src/lib.rs
@@ -0,0 +1,144 @@
+extern crate js_sys;
+extern crate fixedbitset;
+
+mod utils;
+
+use fixedbitset::FixedBitSet;
+use wasm_bindgen::prelude::*;
+
+
+#[wasm_bindgen]
+#[repr(u8)] // Represent each cell as a single byte
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1,
+}
+
+#[wasm_bindgen]
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: FixedBitSet,
+}
+
+impl Universe {
+    /// Gets index of current cell
+    fn get_index(&self, row: u32, column: u32) -> usize {
+        (row * self.width + column) as usize
+    }
+    /// Counts the number of neighbors
+    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+        let mut count = 0;
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
+                let neighbor_row = (row + delta_row) % self.height;
+                let neighbor_col = (column + delta_col) % self.width;
+                let idx = self.get_index(neighbor_row, neighbor_col);
+                count += self.cells[idx] as u8;
+            }
+        }
+        count
+    }
+}
+
+#[wasm_bindgen]
+impl Universe {
+    // Constructor: Initialize the field
+    pub fn new() -> Universe {
+        let width = 64;
+        let height = 64;
+        // Randomly generate initial cells
+        let size = (width*height) as usize;
+        // FixedBitSet can save binary data(fixed bit) per cell.
+        let mut cells = FixedBitSet::with_capacity(size);
+        for i in 0..size {
+            cells.set(i, js_sys::Math::random() < 0.5);
+        }
+        Universe {
+            width,
+            height,
+            cells,
+        }
+    }
+    // Renderer
+    pub fn render(&self) -> String {
+        self.to_string()
+    }
+    // Tick function that determines the next tick (judging live/death of the
+    // given cell by the rule from "game of life")
+    pub fn tick(&mut self) {
+        let mut next = self.cells.clone();
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let idx = self.get_index(row, col);
+                let cell = self.cells[idx];
+                let live_neighbors = self.live_neighbor_count(row, col);
+
+                next.set(idx, match(cell, live_neighbors){
+                    // Rule 1. Any live cell with fewer than two live neighbors
+                    // dies, as if caused by underpopulation
+                    (true, x) if x < 2 => false,
+                    // Rule 2. Any live cell with two or three living neighbors
+                    // lives on to next generation
+                    (true, 2) | (true, 3) => true,
+                    // Rule 3, Any live cell with more than three live neighbors,
+                    // dies, as if by overpopulation
+                    (true, x) if x > 3 => false,
+                    // Rule 4, Any dead cell with exactly three live neighbors
+                    // revives, as if by reproduction
+                    (false, 3) => true,
+                    // All other cells remain in the same state.
+                    (otherwise, _) => otherwise,
+                });
+            }
+        }
+        // Renew by vector
+        self.cells = next;
+    }
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+    pub fn cells(&self) -> *const u32 {
+        self.cells.as_slice().as_ptr()
+    }
+}
+
+use std::fmt;
+
+impl fmt::Display for Universe {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // This will create 2d vector that contains 1d vector slice
+        for line in self.cells.as_slice().chunks(self.width as usize) {
+        for &cell in line {
+                let symbol = if cell == 0 { '◻' } else { '◼' };
+                write!(f, "{}", symbol)?;
+            }
+            write!(f, "\n")?;
+        }
+
+        Ok(())
+    }
+}
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
+}
